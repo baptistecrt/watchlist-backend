052cdc874e37c6901e737258282d63ef3ab9d873
baptistecrt <courtiesbaptiste31@gmail.com>
2026-09-21 09:30:05 +0200

    Update tmdb.js

--- api/tmdb.js
+++ api/tmdb.js
@@ -1,1 +1,23 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  const { endpoint, query } = req.query;
+  const TMDB_KEY = 'b6a1b8cbedd5f34d3b88df2b58e7c1b2';
+
+  try {
+    const url = `https://api.themoviedb.org/3/${endpoint}?api_key=${TMDB_KEY}&${query}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
